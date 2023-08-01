c12916d77b8287e1aa373380757f7e6a4dbd8d5d
mdsrmed <mdsrmed@gmail.com>
2023-08-01 18:51:37 -0400

    Initial Commit

---
+++ iStore/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  iStore
+//
+//  Created by Md Shohidur Rahman on 8/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("iStore")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iStore/iStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  iStoreApp.swift
+//  iStore
+//
+//  Created by Md Shohidur Rahman on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct iStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
